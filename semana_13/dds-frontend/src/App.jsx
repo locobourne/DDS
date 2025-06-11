45f2ffa027e6f1246e58beb7fbb441443e58f81e
Maximo Castillo <403257@sistemas.frc.utn.edu.ar>
2025-06-11 17:40:46 -0300

    Agrego la semana_13 con el paso a paso del frontend adentro (todavia no terminado)

---
+++ semana_13/dds-frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import {Menu} from "./components/Menu";
+import { Footer } from "./components/Footer";
+import Inicio from "./components/Inicio";
+import { Categorias } from "./components/Categorias";
+import { Articulos } from "./components/articulos/Articulos";
+
+
+
+function App() {
+  return (
+    <>
+        <BrowserRouter>
+          <Menu />
+          <div className="divBody">
+            <Routes>
+              <Route path="/inicio" element={<Inicio />} />
+              <Route
+                path="/categorias"
+                element={<Categorias />}
+              />
+              <Route path="*" element={<Navigate to="/inicio" replace />} />
+              <Route path="/articulos" element={<Articulos/>} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+  );
+}
+export default App;
