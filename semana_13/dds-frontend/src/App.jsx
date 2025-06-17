446180f54757194ab725424ea45843c60efbc808
Maximo Castillo <403257@sistemas.frc.utn.edu.ar>
2025-06-16 22:45:25 -0300

    Front totalmente terminado

--- semana_13/dds-frontend/src/App.jsx
+++ semana_13/dds-frontend/src/App.jsx
@@ -7,28 +7,37 @@ import Inicio from "./components/Inicio";
 import { Categorias } from "./components/Categorias";
 import { Articulos } from "./components/articulos/Articulos";
 import { ModalDialog } from "./components/ModalDialog";
+import { Usuarios } from "./components/Usuarios";
+import {RequireAuth} from "./components/RequiereAuth" ;
+import { Login } from "./components/login/Login";
 
 
 
 function App() {
   return (
     <>
-        <BrowserRouter>
-         <ModalDialog/>
-          <Menu />
-          <div className="divBody">
-            <Routes>
-              <Route path="/inicio" element={<Inicio />} />
-              <Route
-                path="/categorias"
-                element={<Categorias />}
-              />
-              <Route path="*" element={<Navigate to="/inicio" replace />} />
-              <Route path="/articulos" element={<Articulos/>} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
+      <BrowserRouter>
+        <ModalDialog />
+        <Menu />
+        <div className="divBody">
+          <Routes>
+            <Route path="/inicio" element={<Inicio />} />
+            <Route path="/categorias" element={<Categorias />} />
+            <Route path="/articulos" element={<Articulos />} />
+            <Route
+              path="/usuarios"
+              element={
+                <RequireAuth>
+                  <Usuarios />
+                </RequireAuth>
+              }
+            />
+            <Route path="/login/:componentFrom" element={<Login />} />
+            <Route path="*" element={<Navigate to="/inicio" replace />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
